690c24b581da4c267416c146b24b2246227ec5a5
AniketGirhe <girheaniket@gmail.com>
2021-09-27 10:00:10 +0530

    code commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: "app-root",
-  templateUrl: "./app.component.html"
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent{
-  pageHeader:string ="Employee Details"
+export class AppComponent {
+  pageHeader: string = "Employee Details"
+  imgPath:string = "https://www.pragimtech.com/wp-content/uploads/2019/02/pargim-logo-1.png";
 }
--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -1,13 +1,42 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector:"employee-details",
-    templateUrl:"./employee-details.component.html",
-    styleUrls:["./employee-details.component.css"]
+    selector: "employee-details",
+    templateUrl: "./employee-details.component.html",
+    styleUrls: ["./employee-details.component.css"]
 })
-export class EmployeeDetailsComponent{
-    name:string="Aniket";
-    lastName:string="Girhe";
-    gender:string="Male";
-    age:number=25;
+export class EmployeeDetailsComponent {
+    columnSpan: number = 2;
+    tableHeader: string = "Employee Details"
+    name: string = "Aniket";
+    lastName: string = "Girhe";
+    gender: string = "Male";
+    age: number = 25;
+
+    
+    classesToApply: string = "boldClass italicClass";
+    applyBoldClass: boolean = true;
+    applyItalicClass: boolean = true;
+    applyColorClass: boolean = true;
+
+    isBold:boolean=true;
+    fontSize:number=30;
+
+    applyClassToButton(): any {
+        let classes = {
+            boldClass: this.applyBoldClass,
+            italicClass: this.applyItalicClass,
+            colorClass: this.applyColorClass
+        }
+        return classes
+    }
+
+    getStyle():any{
+        let styles={
+            'font-size':'30px',
+            'font-weight':'bold',
+            'font-style': 'italic' 
+        }
+        return styles;
+    }
 }
